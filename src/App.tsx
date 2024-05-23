e4f05e8f133ce09a195949424012b58e072a585c
Florian <flo01047@gmail.com>
2024-05-23 17:08:19 +0200

    Project Init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+"use client";
+import "./App.css";
+import { LightSwitch } from "./components/light-switch";
+
+function App() {
+	return (
+		<div className="container">
+			<LightSwitch />
+		</div>
+	);
+}
+
+export default App;
